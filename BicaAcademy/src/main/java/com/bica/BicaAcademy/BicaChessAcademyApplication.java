e4f5b149bcad50ab90d2864c678b6fe99e1089cf
aminekonyali <mohamedamine.konyali@esprit.tn>
2021-10-27 22:09:08 +0100

    first commit

---
+++ BicaAcademy/src/main/java/com/bica/BicaAcademy/BicaChessAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.bica.BicaAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicaChessAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicaChessAcademyApplication.class, args);
+	}
+
+}
---
+++ BicaAcademy/src/test/java/com/bica/BicaAcademy/BicaChessAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bica.BicaAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicaChessAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
